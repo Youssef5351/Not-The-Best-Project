33fe36172a7eeaf3c00f267e541dfc2a77929b7e
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2024-08-20 17:44:12 +0300

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -10,12 +10,12 @@ return [
     | string or array, the allowed_headers option must be an array.
     |
     */
-        'paths' => ['api/*','sanctum/csrf-cookie','api/signup'],
-        'allowed_methods' => ['POST', 'GET', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
-        'allowed_origins' => ['https://vanilla2-gray.vercel.app'],
-        'allowed_origins_patterns' => [],
-        'allowed_headers' => ['*'],
-        'exposed_headers' => ['*'],
-        'max_age' => 0,
-        'supports_credentials' => true,
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['POST', 'GET', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+    'allowed_origins' => ['https://vanilla2-gray.vercel.app'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
 ];
