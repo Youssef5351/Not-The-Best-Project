d2fab3f2a4212d47acfbbc85ff237e5a81b3062a
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2024-08-22 00:03:35 +0300

    Update index.php

--- backend/public/index.php
+++ backend/public/index.php
@@ -11,8 +11,8 @@ define('LARAVEL_START', microtime(true));
 |--------------------------------------------------------------------------
 |
 | If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that the developers may still access it while
-| displaying a message that the site is "down for maintenance".
+| we will load this file so that the developers may still access useful
+| information about the application.
 |
 */
 
@@ -38,7 +38,7 @@ require __DIR__.'/../vendor/autoload.php';
 | Turn On The Lights
 |--------------------------------------------------------------------------
 |
-| We need to illuminate PHP development, so let us turn on the lights.
+| We need to illuminate PHP development, so let's turn on the lights.
 | This bootstraps the framework and gets it ready for use, then it
 | will load up this application so that we can run it and send
 | the responses back to the browser and delight these users.
@@ -46,18 +46,6 @@ require __DIR__.'/../vendor/autoload.php';
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have whipped up for them.
-|
-*/
 
 $kernel = $app->make(Kernel::class);
 
