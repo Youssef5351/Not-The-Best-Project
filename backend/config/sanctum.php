b484f181da604b948f97993fbc1f257ba33fb201
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2024-08-21 20:43:48 +0300

    Update sanctum.php

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
 'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
     '%s%s',
-    'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,vanilla2-gray.vercel.app,vanilla6.vercel.app',
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,vanilla2-gray.vercel.app')),
     Sanctum::currentApplicationUrlWithPort()
 ))),
 
